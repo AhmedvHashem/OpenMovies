e6cf8c75a146f8c9175d194a9dca076ad08d730d
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-16 17:46:17 +0200

    feat: Setup app module
    This commit introduces the initial setup for the app module, including:
    
    -   Added `build.gradle.kts`: Configures the build settings for the app module, including plugins, Android settings, build types, compile options, dependencies, and test configurations.
    -   Added `.gitignore`: Ignores the `/build` directory in the app module.
    -   Added `proguard-rules.pro`: Specifies ProGuard rules for code optimization and obfuscation.
    - add compose, room, retrofit dependencies.

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.hashem.opendictionary"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.banquemisr.challenge05"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+val mockitoAgent = configurations.create("mockitoAgent")
+dependencies {
+    implementation(libs.kotlin.serialization)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.paging)
+    implementation(libs.androidx.paging.compose)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.serialization)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.room)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    debugImplementation(platform(libs.androidx.compose.bom))
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.testing.androidx.compose.manifest)
+
+    testImplementation(libs.testing.junit)
+    testImplementation(libs.testing.kotlin)
+    testImplementation(libs.testing.kotlin.coroutines)
+    testImplementation(libs.mocking.mockito.core)
+    mockitoAgent(libs.mocking.mockito.core) { isTransitive = false }
+    testImplementation(libs.mocking.mockito.kotlin)
+    testImplementation(libs.asserting.truth)
+
+    androidTestImplementation(libs.testing.androidx.junit)
+    androidTestImplementation(libs.testing.androidx.espresso)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.testing.androidx.compose.junit4)
+}
+
+tasks.withType<Test>().configureEach {
+    jvmArgs("-javaagent:${mockitoAgent.asPath}")
+}
