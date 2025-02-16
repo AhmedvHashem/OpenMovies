cae0f68fd284629643a81724cccf68adb03f9166
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-16 17:49:45 +0200

    creating default impl for movies repo

---
+++ app/src/main/java/com/hashem/openmovies/feature/data/DefaultMovieRepository.kt
@@ -0,0 +1,38 @@
+package com.hashem.openmovies.feature.data
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.hashem.openmovies.feature.data.cache.MovieCacheDataSource
+import com.hashem.openmovies.feature.data.models.toMovie
+import com.hashem.openmovies.feature.data.remote.MovieRemoteDataSource
+import com.hashem.openmovies.feature.data.paging.NowPlayingMoviesSource
+import com.hashem.openmovies.feature.domain.repository.MovieRepository
+import com.hashem.openmovies.feature.domain.models.Movie
+import com.hashem.openmovies.feature.domain.models.MovieSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class DefaultMovieRepository(
+    private val cacheDataSource: MovieCacheDataSource,
+    private val remoteDataSource: MovieRemoteDataSource
+) : MovieRepository {
+
+    override fun getMovies(source: MovieSource): Flow<PagingData<Movie>> {
+        return Pager(
+            config = PagingConfig(pageSize = 20),
+            pagingSourceFactory = {
+                when (source) {
+                    MovieSource.NowPlaying -> NowPlayingMoviesSource(remoteDataSource)
+                    MovieSource.Popular -> TODO()
+                    MovieSource.Upcoming -> TODO()
+                }
+            }
+        ).flow.map { pagingData ->
+            pagingData.map {
+                it.toMovie()
+            }
+        }
+    }
+}
