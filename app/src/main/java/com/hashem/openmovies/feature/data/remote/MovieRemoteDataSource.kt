41bf74fdf2c921b668180e20c1b8d8f4ae867f2d
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-16 17:49:32 +0200

    adding remote response model and remote data source

---
+++ app/src/main/java/com/hashem/openmovies/feature/data/remote/MovieRemoteDataSource.kt
@@ -0,0 +1,26 @@
+package com.hashem.openmovies.feature.data.remote
+
+import com.hashem.openmovies.feature.data.remote.models.MoviesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieRemoteDataSource {
+
+    @GET("movie/now_playing")
+    suspend fun getNowPlayerMovies(
+        @Query("page") page: Int = 1,
+        @Query("language") language: String = "en",
+    ): MoviesResponse
+
+    @GET("movie/now_playing")
+    suspend fun getPopularMovies(
+        @Query("page") page: Int = 1,
+        @Query("language") language: String = "en",
+    ): MoviesResponse
+
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies(
+        @Query("page") page: Int = 1,
+        @Query("language") language: String = "en",
+    ): MoviesResponse
+}
