d1c569e402bfadc4a128a35f19b7530232eb29d4
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-23 18:37:13 +0200

    feat: Implement AppErrorView, AppEmptyView, AppLoadingView, and AppNavigationBar
    This commit introduces UI components for error, empty, and loading states, and a navigation bar.
    
    -   Added `AppErrorView`: Displays an error message.
    -   Added `AppEmptyView`: Displays a message when no results are found.
    -   Added `AppLoadingView`: Displays a loading indicator.
    -   Added `AppNavigationBar`: Implements a navigation bar for switching between movie categories.

---
+++ app/src/main/java/com/hashem/openmovies/feature/ui/components/AppErrorView.kt
@@ -0,0 +1,22 @@
+package com.hashem.openmovies.feature.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppErrorView(error: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = error,
+            color = MaterialTheme.colorScheme.error,
+        )
+    }
+}
---
+++ app/src/main/java/com/hashem/openmovies/feature/ui/components/AppNavigationBar.kt
@@ -0,0 +1,54 @@
+package com.hashem.openmovies.feature.ui.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import com.hashem.openmovies.feature.ui.AppRoute
+
+@Composable
+fun AppNavigationBar(
+    navController: NavController,
+    currentRoute: String?,
+    routes: List<AppRoute.Movies>,
+) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground,
+    ) {
+        routes.forEach { appRoute ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        contentDescription = appRoute.label,
+                        imageVector = appRoute.icon,
+                    )
+                },
+                label = {
+                    Text(
+                        text = appRoute.label,
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                },
+                selected = currentRoute?.contains(appRoute.route) == true,
+                onClick = {
+                    navController.navigate(appRoute.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
